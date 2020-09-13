9d030449f81a70045144888d7dacb4fbbde6ef13
Hagai Kalinhoff <hagaik@appcard.com>
2020-09-13 14:27:46 +0300

    add tests using pytest

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import pytest
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    return f'Hi, {name}'  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
