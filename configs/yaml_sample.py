e01bd0c3975d5c055e75c7d1837e68aaaf01213c
Hagai Kalinhoff <hagaik@appcard.com>
2020-09-17 18:53:03 +0300

    add yaml sample for building base structure object add test script to test it fixed layers configuration file
    Issue: #10

--- configs/yaml_sample.py
+++ configs/yaml_sample.py
@@ -1,67 +1,87 @@
 """
 This is a sample script for using 'strictyaml' package
 Maybe use deepdiff for changes check
+
+TODO:
+    1. Create a class for functionality API
+    2. Create an Exceptions for errors
+    3. Make tests
 """
 
-# Reading the whole yaml file and getting it as a long string
-with open('../layers.yaml', 'r') as f:
-    yaml_snippet = f.read()
-
-# importing stringyaml necessary stuff
 from strictyaml import Map, Any, Str, Seq, Optional, load
-
-scheme = Map({
-    'layers': Seq(
-        Map({
-            'name': Str(),
-            Optional('children'): Seq(Any())
-        })
-    )
-})
-
-output_yaml = load(yaml_snippet, scheme)
-print(output_yaml.as_yaml())
-print('-' * 10 + ' ' * 3 + 'ORIGINAL' + ' ' * 3 + '-' * 10)
-print(yaml_snippet)
-
-# using anytree
-from anytree import Node, RenderTree
+from strictyaml.exceptions import YAMLValidationError
+from anytree import Node, RenderTree, AnyNode
 from anytree.importer import DictImporter
-from pprint import pprint
-
-data_yaml = output_yaml.data['layers']
-
-
-def validate_layer_children(chunk, name_memo):
-    for layer in chunk:
-        assert 'name' in layer
-        assert isinstance(layer['name'], str)
-        assert layer['name'] not in name_memo
-        name_memo.append(layer['name'])
-
-        from collections.abc import Sequence
-
-        def _seq_but_not_str(obj):
-            return isinstance(obj, Sequence) and not isinstance(obj, (str, bytes, bytearray))
-
-        if 'children' in layer:
-            assert _seq_but_not_str(layer['children'])
-            validate_layer_children(layer['children'], name_memo)
+from collections.abc import Sequence
+from os import path
 
 
-validate_layer_children(data_yaml, [])
-
-pprint(data_yaml)
-global_dict = dict()
-global_dict['name'] = 'global'
-global_dict['children'] = output_yaml.data['layers']
-pprint('-' * 10 + 'Global Print')
-pprint(global_dict)
+class BadYAMLError(AssertionError):
+    # TODO: add informative error cause
+    pass
 
 
-class MyNode(Node):
-    separator = u' \u27f6 '
+class Structure:
+    __scheme = Map({
+        'app': Str(),
+        'layers': Seq(
+            Map({
+                'name': Str(),
+                Optional('children'): Seq(Any())
+            })
+        )
+    })
 
+    def __init__(self):
+        self.__file = None
+        self.__global_layer = None
 
-root2 = DictImporter().import_(global_dict)
-print(RenderTree(root2))
+    def structure_from_file(self, absolute_file_path: str):
+        self.__file_exist(absolute_file_path)
+        try:
+            with open(absolute_file_path, 'r') as file:
+                raw_yaml = file.read()
+            output_yaml = load(raw_yaml, self.__scheme)
+            self.__validate_layer_children(output_yaml.data['layers'], [])
+        except YAMLValidationError as ye:
+            # TODO log error, handle more error types
+            raise BadYAMLError
+        except AssertionError as ae:
+            raise BadYAMLError
+
+        global_dict = dict()
+        global_dict['name'] = 'global'
+        global_dict['children'] = output_yaml.data['layers']
+
+        self.__file = absolute_file_path
+        self.__global_layer = DictImporter().import_(global_dict)
+
+        return self
+
+    def show_structure(self) -> str:
+        if self.__global_layer:
+            return str(RenderTree(self.__global_layer))
+        return 'Structure yet to be defined.'
+
+    @classmethod
+    def __file_exist(cls, file_path: str):
+        if not isinstance(file_path, str) or not file_path.endswith(('.yml', '.yaml')):
+            # TODO: add exception reason
+            raise BadYAMLError()
+        if not path.isfile(file_path):
+            raise BadYAMLError()
+
+    @classmethod
+    def __validate_layer_children(cls, chunk, name_memo):
+        for layer in chunk:
+            assert 'name' in layer
+            assert isinstance(layer['name'], str)
+            assert layer['name'] not in name_memo
+            name_memo.append(layer['name'])
+
+            def _seq_but_not_str(obj):
+                return isinstance(obj, Sequence) and not isinstance(obj, (str, bytes, bytearray))
+
+            if 'children' in layer:
+                assert _seq_but_not_str(layer['children'])
+                cls.__validate_layer_children(layer['children'], name_memo)
--- tests/wallet_test.py
+++ tests/wallet_test.py
@@ -1,28 +1,34 @@
 import pytest
 from configs.models.Wallet import Wallet, InsufficientAmount
 
+
 def test_default_initial_amount():
     wallet = Wallet()
     assert wallet.balance == 0
 
+
 def test_setting_initial_amount():
     wallet = Wallet(100)
     assert wallet.balance == 100
+
 
 def test_wallet_add_cash():
     wallet = Wallet(10)
     wallet.add_cash(90)
     assert wallet.balance == 100
 
+
 def test_wallet_spend_cash():
     wallet = Wallet(20)
     wallet.spend_cash(10)
     assert wallet.balance == 10
 
+
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount():
     wallet = Wallet()
     with pytest.raises(InsufficientAmount):
         wallet.spend_cash(100)
+
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount_second():
     wallet = Wallet()
