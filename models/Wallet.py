e77ca5a57e8556e0c85e83f5c045a7558770b695
Hagai Kalinhoff <hagaik@appcard.com>
2020-10-07 12:05:01 +0300

    added poetry support for existing projects. fixed .travis.yaml file to support changes and preform extra steps add pre-commit hooks: add black support add flake8 support add mypy support add isort support
    configured some in pyproject.toml file

--- models/Wallet.py
+++ models/Wallet.py
@@ -3,13 +3,12 @@ class InsufficientAmount(Exception):
 
 
 class Wallet(object):
-
     def __init__(self, initial_amount=0):
         self.balance = initial_amount
 
     def spend_cash(self, amount):
         if self.balance < amount:
-            raise InsufficientAmount('Not enough available to spend {}'.format(amount))
+            raise InsufficientAmount("Not enough available to spend {}".format(amount))
         self.balance -= amount
 
     def add_cash(self, amount):
